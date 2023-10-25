4c4ae5be0efa97ad86888ef2fde4bf65144b985a
JakeSchreck <schreckj@ufl.edu>
2023-10-25 15:05:09 -0700

    adding code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+#hello added by Jacob Schreck
+def decode(en_pass):
+    decoded_str = ""
+    decoded_list = [int(i) -3 for i in en_pass]
+    decoded_str_list = map(str, decoded_list)
+    for i in decoded_str_list:
+        decoded_str += i
+    return decoded_str
 
 def encoder(input_string):  # This is the encoder function
 
