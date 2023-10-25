0890eec922695d84e253192d72b4d1e20abcece9
Sophia-CardonaN <cardonanader.s@ufl.edu>
2023-10-25 14:37:35 -0400

    adding code

---
+++ main.py
@@ -0,0 +1,42 @@
+
+
+def encoder(input_string):  # This is the encoder function
+
+    if len(input_string) != 8 or not input_string.isdigit():
+        return "Invalid input."
+
+    encoded_password = ""
+
+    for digit in input_string:
+        encoded_digit = (int(digit + 3) % 10)
+        encoded_password = str(encoded_digit)
+
+    return encoded_password
+
+
+def main():
+
+    encoded = None
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        menu_selection = int(input('Please enter an option: '))
+        if menu_selection == 1:
+            input_string = input('Please enter your password to encode: ')
+            encoded = encoder(input_string)
+
+        elif menu_selection == 2:
+            decoded = decoder(encoded)
+            print('The encoded password is ' + 'and the original password is ' + decoded + ".")
+
+        elif menu_selection == 3:
+            break
+
+
+if __name__ == "main":
+    main()
